ad0202df328395100f9d34e75b03d3077f55ac5f
Michael Boutillier <michael.boutillier@gmail.com>
2014-03-10 11:54:38 +0100

    add blueprint of test... juste need to fil it up now...

---
+++ libnessus/test/test_report.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from libnessus.objects import NessusReport
+from test_nessus import TestNessus
+
+
+class TestNessusReport(TestNessus):
+    """Test Report object"""
+
+    def test_hosts(self):
+        """"""
+    def test_save(self):
+        """"""
+    def test_diff(self):
+        """"""
+    def test_started(self):
+        """"""
+    def test_commandline(self):
+        """"""
+    def test_version(self):
+        """"""
+    def test_scan_type(self):
+        """"""
+    def test_get_host_byid(self):
+        """"""
+    def test_endtime(self):
+        """"""
+    def test_summary(self):
+        """"""
+    def test_elapsed(self):
+        """"""
+    def test_hosts_up(self):
+        """"""
+    def test_hosts_down(self):
+        """"""
+    def test_hosts_total(self):
+        """"""
+    def test_get_raw_data(self):
+        """"""
---
+++ libnessus/test/test_vuln.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from test_nessus import TestNessus
+
+
+class TestVuln(TestNessus):
+    """Unit test of vuln object"""
+    def test_port(self):
+        """"""
+    def test_service(self):
+        """"""
+    def test_protocol(self):
+        """"""
+    def test_severity(self):
+        """"""
+    def test_plugin_id(self):
+        """"""
+    def test_plugin_name(self):
+        """"""
+    def test_plugin_family(self):
+        """"""
+    def test_get_vuln_info(self):
+        """"""
+    def test_get_vuln_risk(self):
+        """"""
+    def test_get_vuln_plugin(self):
+        """"""
+    def test_get_vuln_xref(self):
+        """"""
+    def test_synopsis(self):
+        """"""
+    def test_description(self):
+        """"""
+    def test_solution(self):
+        """"""
