e1bfcfe1a35c7c26e2719c7d6c617d838a5d2e59
Mike Boutillier <michael.boutillier@gmail.com>
2014-04-09 13:02:11 +0000

    Minimal test on the Vuln class + correct a few typo in sample

--- libnessus/objects/vuln.py
+++ libnessus/objects/vuln.py
@@ -29,52 +29,108 @@ class NessusVuln(object):
 
     @property
     def service(self):
+        """
+        Get the service name if known
+        :return str
+        """
         return self.__vuln_info['svc_name']
 
     @property
     def protocol(self):
+        """
+        Get the protocol
+        :return str
+        """
         return self.__vuln_info['protocol']
 
     @property
     def severity(self):
+        """
+        Get Severity level, It corresponds with a
+        "0" as an open port,
+        "1" as a low or informational
+        "2" as a medium or warning and
+        "3" as a high or hole
+        return str
+        """
         return self.__vuln_info['severity']
 
     @property
     def plugin_id(self):
+        """
+        Get the plugin id
+        :return str
+        """
         return self.__vuln_info['plugin']['plugin_id']
 
     @property
     def plugin_name(self):
+        """
+        Get the plugin Name
+        :return str
+        """
         return self.__vuln_info['plugin']['plugin_name']
 
     @property
     def plugin_family(self):
+        """
+        Get the test Family
+        :return str
+        """
         return self.__vuln_info['plugin']['plugin_family']
 
     @property
     def get_vuln_info(self):
+        """
+        Get a dict of the whole vulnerability
+        :return dict
+        """
         return self.__vuln_info
 
     @property
     def get_vuln_risk(self):
+        """
+        Get a dict of the risk
+        :return dict
+        """
         return self.__vuln_info['risk_score']
 
     @property
     def get_vuln_plugin(self):
+        """
+        Get a dict of plugin
+        :return dict
+        """
         return self.__vuln_info['plugin']
 
     @property
     def get_vuln_xref(self):
+        """
+        Get a dict of xref
+        :return dict
+        """
         return self.__vuln_info['vuln_ref']
 
     @property
     def synopsis(self):
+        """
+        Get the sypnosis of the vuln
+        :return str
+        """
         return self.__vuln_info['synopsis']
 
     @property
     def description(self):
+        """
+        Get description
+        :return str
+        """
         return self.__vuln_info['description']
 
     @property
     def solution(self):
+        """
+        Get the sulution provide by nessus
+        :return str
+        """
         return self.__vuln_info['solution']
--- libnessus/test/test_vuln.py
+++ libnessus/test/test_vuln.py
@@ -15,28 +15,120 @@ class TestVuln(TestNessus):
             i = i + 1
 
     def test_service(self):
-        """"""
+        """test the service name"""
+        i = 0
+        for vuln in self.VulnList:
+            value = vuln.service
+            expected = self.expected_vuln[i]['svc_name']
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_protocol(self):
-        """"""
+        """test the protocol"""
+        i = 0
+        for vuln in self.VulnList:
+            value = vuln.protocol
+            expected = self.expected_vuln[i]['protocol']
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_severity(self):
-        """"""
+        """test severity"""
+        i = 0
+        for vuln in self.VulnList:
+            value = vuln.severity
+            expected = self.expected_vuln[i]['severity']
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_plugin_id(self):
-        """"""
+        """test plugin id"""
+        i = 0
+        for vuln in self.VulnList:
+            value = vuln.plugin_id
+            expected = self.expected_vuln[i]['plugin_id']
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_plugin_name(self):
-        """"""
+        """test plugin name"""
+        i = 0
+        for vuln in self.VulnList:
+            value = vuln.plugin_name
+            expected = self.expected_vuln[i]['plugin_name']
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_plugin_family(self):
-        """"""
+        """test plugin family"""
+        i = 0
+        for vuln in self.VulnList:
+            value = vuln.plugin_family
+            expected = self.expected_vuln[i]['plugin_family']
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_get_vuln_info(self):
-        """"""
+        """test return type of vuln_info"""
+        i = 0
+        for vuln in self.VulnList:
+            value = isinstance(vuln.get_vuln_info, dict)
+            expected = True
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_get_vuln_risk(self):
-        """"""
+        """test return type of risk factor"""
+        i = 0
+        for vuln in self.VulnList:
+            value = isinstance(vuln.get_vuln_risk, dict)
+            expected = True
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_get_vuln_plugin(self):
-        """"""
+        """
+        test return type of plugin dict
+        """
+        i = 0
+        for vuln in self.VulnList:
+            value = isinstance(vuln.get_vuln_plugin, dict)
+            expected = True
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_get_vuln_xref(self):
-        """"""
+        """test return type of xref"""
+        i = 0
+        for vuln in self.VulnList:
+            value = isinstance(vuln.get_vuln_xref, dict)
+            expected = True
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_synopsis(self):
-        """"""
+        """test synopsis"""
+        i = 0
+        for vuln in self.VulnList:
+            value = vuln.synopsis
+            expected = self.expected_vuln[i]['synopsis']
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_description(self):
-        """"""
+        """test return type od descr"""
+        i = 0
+        for vuln in self.VulnList:
+            value = isinstance(vuln.description, str)
+            expected = True
+            self.assertEqual(value, expected)
+            i = i + 1
+
     def test_solution(self):
-        """"""
+        """test solution string"""
+        i = 0
+        for vuln in self.VulnList:
+            value = vuln.solution
+            expected = self.expected_vuln[i]['solution']
+            self.assertEqual(value, expected)
+            i = i + 1
