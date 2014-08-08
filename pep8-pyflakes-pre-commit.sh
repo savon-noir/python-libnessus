adc13acf80935b2144a50a1b470df1edbb73beb7
Mike Boutillier <michael.boutillier@gmail.com>
2014-08-08 15:57:25 +0000

    add comment to precomit hook

--- pep8-pyflakes-pre-commit.sh
+++ pep8-pyflakes-pre-commit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # run pyflakes on all the python source files in the repo
+# run this to make it on precommit :
+# ln -s pep8-pyflakes-pre-commit.sh .git/hooks/pre-commit
 #FAULTS=$(find ./* -iname "*.py" -exec pyflakes {} \; 2>&1)
 pyflakes .
 FAULTS=$?
