813da30dbe2889066b75b746ec5bc29e2bef8f9b
Michael Boutillier <michael.boutillier@ingenico.com>
2019-08-16 00:21:18 +0200

    correct setup.py....

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup, find_packages
 
-with open("README.rst") as rfile:
+with open("README.md") as rfile:
     long_description = rfile.read()
 
 setup(
     name='python-libnessus',
-    version='1.0.0.3',
+    version='1.0.0.9',
     author='Ronald Bister and Mike Boutillier',
-    author_email='mini.pelle@gmail.com/michael.boutillier@gmail.com',
+    author_email='michael.boutillier@gmail.com',
     packages=find_packages(exclude=["*.test"]),
     install_requires=[
-            'jsonpickle',
-        ],
+            'jsonpickle', ],
     url='https://github.com/bmx0r/python-libnessus',
-    download_url='https://github.com/bmx0r/python-libnessus/tarball/1.0.0.2',
+    download_url='https://github.com/bmx0r/python-libnessus/tarball/1.0.0.9',
     license='Creative Common "Attribution" license (CC-BY) v3',
     description=('Python Nessus module to parse, chat with XMLRPC API, ...'),
     long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=["Development Status :: 4 - Beta",
                  "Environment :: Console",
-                 "Programming Language :: Python :: 2.6",
                  "Programming Language :: Python :: 2.7",
-                 "Programming Language :: Python :: 3.3",
+                 "Programming Language :: Python :: 3.4",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
                  "Topic :: System :: Networking"]
 )
