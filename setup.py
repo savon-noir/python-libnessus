8c022fd51e2cfca96b9889780791f91d4352a5b9
Michael Boutillier <michael.boutillier@ingenico.com>
2019-08-29 09:03:35 +0200

    review setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-import os
 from setuptools import setup, find_packages
 
-if os.path.isfile('README.md'):
-    with open("README.md") as rfile:
-        long_decription = rfile.read()
-        long_description_content_type = 'text/markdown'
-elif os.path.isfile('README.rst'):
-    with open("README.rst") as rfile:
-        long_description = rfile.read()
-        long_description_content_type = 'text/x-rst'
-else:
-    long_description = ''
+# if os.path.isfile('README.md'):
+with open("README.md") as rfile:
+    long_decription = rfile.read()
     long_description_content_type = 'text/markdown'
+# elif os.path.isfile('README.rst'):
+#    with open("README.rst") as rfile:
+#        long_description = rfile.read()
+#        long_description_content_type = 'text/x-rst'
+# else:
+#    long_description = ''
+#    long_description_content_type = 'text/markdown'
 
 setup(
     name='python-libnessus',
@@ -25,7 +24,7 @@ setup(
     download_url='https://github.com/bmx0r/python-libnessus/tarball/1.0.0.9',
     license='Creative Common "Attribution" license (CC-BY) v3',
     description=('Python Nessus module to parse, chat with XMLRPC API, ...'),
-    long_description=long_description,
+    long_description=long_decription,
     long_description_content_type=long_description_content_type,
     classifiers=["Development Status :: 4 - Beta",
                  "Environment :: Console",
